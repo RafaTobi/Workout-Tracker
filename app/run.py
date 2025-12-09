4c3139ef51d245d117893fcc27a9e2e7652bb4ea
RafaTobi <t.rafael.obias@gmail.com>
2025-12-09 17:09:01 +0100

    chore: seed exercise data

--- app/run.py
+++ app/run.py
@@ -1,5 +1,6 @@
 import uvicorn
-
+from app.db.data.seed import seed_exercise_data
 
 if __name__ == '__main__':
+    seed_exercise_data()
     uvicorn.run('app.main:app', host='127.0.0.1', port=8000, reload=True)
